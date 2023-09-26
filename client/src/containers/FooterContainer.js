acc1fc1dd6b3f4754effa9c41236116fc37fd8d6
M <mrgmckenzie@gmail.com>
2023-09-26 12:55:10 +0100

    fixed link display

--- client/src/containers/FooterContainer.js
+++ client/src/containers/FooterContainer.js
@@ -4,7 +4,7 @@ const FooterContainer = () => {
 
     return (
         <div className="footer-container">
-            <a className="footer-text" href="https://open-meteo.com/">Weather data by Open-Meteo.com</a>
+            <a className="footer-link" href="https://open-meteo.com/">Weather data by Open-Meteo.com</a>
             <p className="footer-text">© 2023 Mahzabin, Daniel & Michael.</p>
         </div>
     )
